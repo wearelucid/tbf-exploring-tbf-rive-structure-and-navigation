4cbe491ebc49fb50249cf4dff91af05931b788fe
TobiasDupuch <tobias.dupuch@wearelucid.ch>
2025-11-17 16:46:48 +0100

    feat: add structure object and rename old view-model structure

---
+++ structure.js
@@ -0,0 +1,382 @@
+const DISTRICTS_CONTAINERS = {
+    "01": {
+        visibleBooleanPath: "districtContainers/districtContainer01/visible",
+        district: {
+            reference: "districtKVA",
+            title: { "deCH": "KVA" },
+            subtitle: { "deCH": "" },
+        },
+        scenes: [
+            {
+                reference: "kvaScene01",
+                title: { "deCH": "Scene 01" },
+                description: { "deCH": "CH-Deutsche description der Szene01" },
+                clickedTriggerPath: "districts/districtKVA/kvaScene01/clicked"
+            },
+            {
+                reference: "kvaScene02",
+                title: { "deCH": "Scene 02" },
+                description: { "deCH": "CH-Deutsche description der Szene02" },
+                clickedTriggerPath: "districts/districtKVA/kvaScene02/clicked"
+            },
+            {
+                reference: "kvaScene03",
+                title: { "deCH": "Scene 03" },
+                description: { "deCH": "CH-Deutsche description der Szene03" },
+                clickedTriggerPath: "districts/districtKVA/kvaScene03/clicked"
+            },
+            {
+                reference: "kvaScene04",
+                title: { "deCH": "Scene 04" },
+                description: { "deCH": "CH-Deutsche description der Szene04" },
+                clickedTriggerPath: "districts/districtKVA/kvaScene04/clicked"
+            }
+        ]
+    },
+    "02": {
+        visibleBooleanPath: "districtContainers/districtContainer02/visible",
+        district: {
+            reference: "districtAgglomeration",
+            title: { "deCH": "Agglomeration" },
+            subtitle: { "deCH": "" },
+        },
+        scenes: [
+            {
+                reference: "agglomerationScene01",
+                title: { "deCH": "Scene 01" },
+                description: { "deCH": "CH-Deutsche description der Szene01" },
+                clickedTriggerPath: "districts/districtAgglomeration/agglomerationScene01/clicked"
+            },
+            {
+                reference: "agglomerationScene02",
+                title: { "deCH": "Scene 02" },
+                description: { "deCH": "CH-Deutsche description der Szene02" },
+                clickedTriggerPath: "districts/districtAgglomeration/agglomerationScene02/clicked"
+            },
+            {
+                reference: "agglomerationScene03",
+                title: { "deCH": "Scene 03" },
+                description: { "deCH": "CH-Deutsche description der Szene03" },
+                clickedTriggerPath: "districts/districtAgglomeration/agglomerationScene03/clicked"
+            }
+        ]
+    },
+    "03": {
+        visibleBooleanPath: "districtContainers/districtContainer03/visible",
+        district: {
+            reference: "districtUmland",
+            title: { "deCH": "Umland" },
+            subtitle: { "deCH": "" },
+        },
+        scenes: [
+            {
+                reference: "umlandScene01",
+                title: { "deCH": "Scene 01" },
+                description: { "deCH": "CH-Deutsche description der Szene01" },
+                clickedTriggerPath: "districts/districtUmland/umlandScene01/clicked"
+            },
+            {
+                reference: "umlandScene02",
+                title: { "deCH": "Scene 02" },
+                description: { "deCH": "CH-Deutsche description der Szene02" },
+                clickedTriggerPath: "districts/districtUmland/umlandScene02/clicked"
+            },
+            {
+                reference: "umlandScene03",
+                title: { "deCH": "Scene 03" },
+                description: { "deCH": "CH-Deutsche description der Szene03" },
+                clickedTriggerPath: "districts/districtUmland/umlandScene03/clicked"
+            }
+        ]
+    },
+    "04": {
+        visibleBooleanPath: "districtContainers/districtContainer04/visible",
+        district: {
+            reference: "districtFluss",
+            title: { "deCH": "Fluss" },
+            subtitle: { "deCH": "" },
+        },
+        scenes: [
+            {
+                reference: "flussScene01",
+                title: { "deCH": "Scene 01" },
+                description: { "deCH": "CH-Deutsche description der Szene01" },
+                clickedTriggerPath: "districts/districtFluss/flussScene01/clicked"
+            },
+            {
+                reference: "flussScene02",
+                title: { "deCH": "Scene 02" },
+                description: { "deCH": "CH-Deutsche description der Szene02" },
+                clickedTriggerPath: "districts/districtFluss/flussScene02/clicked"
+            },
+            {
+                reference: "flussScene03",
+                title: { "deCH": "Scene 03" },
+                description: { "deCH": "CH-Deutsche description der Szene03" },
+                clickedTriggerPath: "districts/districtFluss/flussScene03/clicked"
+            }
+        ]
+    },
+    "05": {
+        visibleBooleanPath: "districtContainers/districtContainer05/visible",
+        district: {
+            reference: "districtIndustrie",
+            title: { "deCH": "Industrie" },
+            subtitle: { "deCH": "" },
+        },
+        scenes: [
+            {
+                reference: "industrieScene01",
+                title: { "deCH": "Scene 01" },
+                description: { "deCH": "CH-Deutsche description der Szene01" },
+                clickedTriggerPath: "districts/districtIndustrie/industrieScene01/clicked"
+            }
+        ]
+    },
+    "06": {
+        visibleBooleanPath: "districtContainers/districtContainer06/visible",
+        district: {
+            reference: "districtVorstadt",
+            title: { "deCH": "Vorstadt" },
+            subtitle: { "deCH": "" },
+        },
+        scenes: [
+            {
+                reference: "vorstadtScene01",
+                title: { "deCH": "Scene 01" },
+                description: { "deCH": "CH-Deutsche description der Szene01" },
+                clickedTriggerPath: "districts/districtVorstadt/vorstadtScene01/clicked"
+            },
+            {
+                reference: "vorstadtScene02",
+                title: { "deCH": "Scene 02" },
+                description: { "deCH": "CH-Deutsche description der Szene02" },
+                clickedTriggerPath: "districts/districtVorstadt/vorstadtScene02/clicked"
+            },
+            {
+                reference: "vorstadtScene03",
+                title: { "deCH": "Scene 03" },
+                description: { "deCH": "CH-Deutsche description der Szene03" },
+                clickedTriggerPath: "districts/districtVorstadt/vorstadtScene03/clicked"
+            }
+        ]
+    },
+    "07": {
+        visibleBooleanPath: "districtContainers/districtContainer07/visible",
+        district: {
+            reference: "districtStadtpark",
+            title: { "deCH": "Stadtpark" },
+            subtitle: { "deCH": "" },
+        },
+        scenes: [
+            {
+                reference: "stadtparkScene01",
+                title: { "deCH": "Scene 01" },
+                description: { "deCH": "CH-Deutsche description der Szene01" },
+                clickedTriggerPath: "districts/districtStadtpark/stadtparkScene01/clicked"
+            },
+            {
+                reference: "stadtparkScene02",
+                title: { "deCH": "Scene 02" },
+                description: { "deCH": "CH-Deutsche description der Szene02" },
+                clickedTriggerPath: "districts/districtStadtpark/stadtparkScene02/clicked"
+            },
+            {
+                reference: "stadtparkScene03",
+                title: { "deCH": "Scene 03" },
+                description: { "deCH": "CH-Deutsche description der Szene03" },
+                clickedTriggerPath: "districts/districtStadtpark/stadtparkScene03/clicked"
+            }
+        ]
+    },
+    "08": {
+        visibleBooleanPath: "districtContainers/districtContainer08/visible",
+        district: {
+            reference: "districtInnenstadt",
+            title: { "deCH": "Innenstadt" },
+            subtitle: { "deCH": "" },
+        },
+        scenes: [
+            {
+                reference: "innenstadtScene01",
+                title: { "deCH": "Scene 01" },
+                description: { "deCH": "CH-Deutsche description der Szene01" },
+                clickedTriggerPath: "districts/districtInnenstadt/innenstadtScene01/clicked"
+            },
+            {
+                reference: "innenstadtScene02",
+                title: { "deCH": "Scene 02" },
+                description: { "deCH": "CH-Deutsche description der Szene02" },
+                clickedTriggerPath: "districts/districtInnenstadt/innenstadtScene02/clicked"
+            },
+            {
+                reference: "innenstadtScene03",
+                title: { "deCH": "Scene 03" },
+                description: { "deCH": "CH-Deutsche description der Szene03" },
+                clickedTriggerPath: "districts/districtInnenstadt/innenstadtScene03/clicked"
+            }
+        ]
+    },
+    "09": {
+        visibleBooleanPath: "districtContainers/districtContainer09/visible",
+        district: {
+            reference: "districtMysterium",
+            title: { "deCH": "Mysterium" },
+            subtitle: { "deCH": "" },
+        },
+        scenes: [
+            {
+                reference: "mysteriumScene01",
+                title: { "deCH": "Scene 01" },
+                description: { "deCH": "CH-Deutsche description der Szene01" },
+                clickedTriggerPath: "districts/districtMysterium/mysteriumScene01/clicked"
+            },
+            {
+                reference: "mysteriumScene02",
+                title: { "deCH": "Scene 02" },
+                description: { "deCH": "CH-Deutsche description der Szene02" },
+                clickedTriggerPath: "districts/districtMysterium/mysteriumScene02/clicked"
+            },
+            {
+                reference: "mysteriumScene03",
+                title: { "deCH": "Scene 03" },
+                description: { "deCH": "CH-Deutsche description der Szene03" },
+                clickedTriggerPath: "districts/districtMysterium/mysteriumScene03/clicked"
+            }
+        ]
+    },
+    "10": {
+        visibleBooleanPath: "districtContainers/districtContainer10/visible",
+        district: {
+            reference: "districtBahnhof",
+            title: { "deCH": "Bahnhof" },
+            subtitle: { "deCH": "" },
+        },
+        scenes: [
+            {
+                reference: "bahnhofScene01",
+                title: { "deCH": "Scene 01" },
+                description: { "deCH": "CH-Deutsche description der Szene01" },
+                clickedTriggerPath: "districts/districtBahnhof/bahnhofScene01/clicked"
+            },
+            {
+                reference: "bahnhofScene02",
+                title: { "deCH": "Scene 02" },
+                description: { "deCH": "CH-Deutsche description der Szene02" },
+                clickedTriggerPath: "districts/districtBahnhof/bahnhofScene02/clicked"
+            },
+            {
+                reference: "bahnhofScene03",
+                title: { "deCH": "Scene 03" },
+                description: { "deCH": "CH-Deutsche description der Szene03" },
+                clickedTriggerPath: "districts/districtBahnhof/bahnhofScene03/clicked"
+            },
+            {
+                reference: "bahnhofScene04",
+                title: { "deCH": "Scene 04" },
+                description: { "deCH": "CH-Deutsche description der Szene04" },
+                clickedTriggerPath: "districts/districtBahnhof/bahnhofScene04/clicked"
+            }
+        ]
+    },
+    "11": {
+        visibleBooleanPath: "districtContainers/districtContainer11/visible",
+        district: {
+            reference: "districtEntwicklungsgebiet",
+            title: { "deCH": "Entwicklungsgebiet" },
+            subtitle: { "deCH": "" },
+        },
+        scenes: [
+            {
+                reference: "entwicklungsgebietScene01",
+                title: { "deCH": "Scene 01" },
+                description: { "deCH": "CH-Deutsche description der Szene01" },
+                clickedTriggerPath: "districts/districtEntwicklungsgebiet/entwicklungsgebietScene01/clicked"
+            },
+            {
+                reference: "entwicklungsgebietScene02",
+                title: { "deCH": "Scene 02" },
+                description: { "deCH": "CH-Deutsche description der Szene02" },
+                clickedTriggerPath: "districts/districtEntwicklungsgebiet/entwicklungsgebietScene02/clicked"
+            }
+        ]
+    },
+    "12": {
+        visibleBooleanPath: "districtContainers/districtContainer12/visible",
+        district: {
+            reference: "districtFinanzdistrikt",
+            title: { "deCH": "Finanzdistrikt" },
+            subtitle: { "deCH": "" },
+        },
+        scenes: [
+            {
+                reference: "finanzdistrictScene01",
+                title: { "deCH": "Scene 01" },
+                description: { "deCH": "CH-Deutsche description der Szene01" },
+                clickedTriggerPath: "districts/districtFinanzdistrikt/finanzdistrictScene01/clicked"
+            },
+            {
+                reference: "finanzdistrictScene02",
+                title: { "deCH": "Scene 02" },
+                description: { "deCH": "CH-Deutsche description der Szene02" },
+                clickedTriggerPath: "districts/districtFinanzdistrikt/finanzdistrictScene02/clicked"
+            }
+        ]
+    },
+    "13": {
+        visibleBooleanPath: "districtContainers/districtContainer13/visible",
+        district: {
+            reference: "districtHochhausquartier",
+            title: { "deCH": "Hochhausquartier" },
+            subtitle: { "deCH": "" },
+        },
+        scenes: [
+            {
+                reference: "hochhausquartierScene01",
+                title: { "deCH": "Scene 01" },
+                description: { "deCH": "CH-Deutsche description der Szene01" },
+                clickedTriggerPath: "districts/districtHochhausquartier/hochhausquartierScene01/clicked"
+            },
+            {
+                reference: "hochhausquartierScene02",
+                title: { "deCH": "Scene 02" },
+                description: { "deCH": "CH-Deutsche description der Szene02" },
+                clickedTriggerPath: "districts/districtHochhausquartier/hochhausquartierScene02/clicked"
+            },
+            {
+                reference: "hochhausquartierScene03",
+                title: { "deCH": "Scene 03" },
+                description: { "deCH": "CH-Deutsche description der Szene03" },
+                clickedTriggerPath: "districts/districtHochhausquartier/hochhausquartierScene03/clicked"
+            }
+        ]
+    },
+    "14": {
+        visibleBooleanPath: "districtContainers/districtContainer14/visible",
+        district: {
+            reference: "districtFlughafen",
+            title: { "deCH": "Flughafen" },
+            subtitle: { "deCH": "" },
+        },
+        scenes: [
+            {
+                reference: "flughafenScene01",
+                title: { "deCH": "Scene 01" },
+                description: { "deCH": "CH-Deutsche description der Szene01" },
+                clickedTriggerPath: "districts/districtFlughafen/flughafenScene01/clicked"
+            },
+            {
+                reference: "flughafenScene02",
+                title: { "deCH": "Scene 02" },
+                description: { "deCH": "CH-Deutsche description der Szene02" },
+                clickedTriggerPath: "districts/districtFlughafen/flughafenScene02/clicked"
+            },
+            {
+                reference: "flughafenScene03",
+                title: { "deCH": "Scene 03" },
+                description: { "deCH": "CH-Deutsche description der Szene03" },
+                clickedTriggerPath: "districts/districtFlughafen/flughafenScene03/clicked"
+            }
+        ]
+    }
+};
